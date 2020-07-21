688531947c6593b67f62f6c9ef76cb87c7aea80b
Chris Cooney <chris.cooney05@hotmail.co.uk>
2020-07-21 11:47:51 +0100

    added element to canvas in a fixed possition + added a border

---
+++ src/index.js
@@ -0,0 +1,5 @@
+const canvas = document.getElementById("game_area");
+const ctx = canvas.getContext("2d");
+//.getContext() gives us the internal rendering context so we can draw on our canvas
+
+ctx.fillRect(20, 20, 100, 100);
